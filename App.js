bf14a855bfe991263382b487f0f7dca36fcaf91a
PabloSanchezIrigoyen <pabloomanuel13@gmail.com>
2025-03-07 10:44:08 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import Ahorros from './screens/ahorros';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="Ahorros" component={Ahorros} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
